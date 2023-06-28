22e2119c140ac5dcaca2734dc22619ea2d36a844
Oksana Karpyn <oksana101992@gmail.com>
2023-06-29 00:14:37 +0200

    add card

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     //
     public function index(){
        // $post = Project::with('category','tecnologies')->get();
-        $post = Project::with('category','tecnologies')->get();
+        $post = Project::with('category','tecnologies')->paginate(4);
         return response()->json([
             'success'=> true,
             'post'=> $post
